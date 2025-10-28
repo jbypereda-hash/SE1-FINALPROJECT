a2559542ac3383d9b8ab8b2c93aaec0869ae4b7e
Yen Vasquez <yenbvasquez@gmail.com>
2025-10-28 13:50:14 +0800

    meow

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,7 +3,6 @@ import Button from "./Button";
 const NavigationBar = () => {
   return (
     <nav className="flex items-center justify-between bg-black-34 text-white px-8 py-4">
-      
       {/* Left Side - CORELAB LOGO - Home Button*/}
       <div className="font-sans font-bold text-shrekz">
         <Button to="/">CORELAB</Button>
@@ -12,7 +11,7 @@ const NavigationBar = () => {
         <Button to="/">HOME</Button>
         <Button to="/memberships">MEMBERSHIPS</Button>
         <Button>CLASSES</Button>
-        <Button>COACHES</Button>
+        <Button>mama</Button>
       </div>
       <div>
         <Button>MY PROFILE</Button>
