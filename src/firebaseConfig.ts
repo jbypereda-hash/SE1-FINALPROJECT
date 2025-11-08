1f4ce9b41b1e5e6b8474d23916e0c005ef185fcf
cheriestrera <cheriestrera@gmail.com>
2025-11-08 14:52:33 +0800

    setting up firebase [wala pa na done]

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDW2eI26806p45Pk31Tk3PHu3VDTybGXNQ",
+  authDomain: "corelab-c3668.firebaseapp.com",
+  projectId: "corelab-c3668",
+  storageBucket: "corelab-c3668.firebasestorage.app",
+  messagingSenderId: "988407391761",
+  appId: "1:988407391761:web:91bb60aa36962f9a75b3b0",
+  measurementId: "G-2ME0CQW9MS",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export Firestore and Storage references
+export const db = getFirestore(app);
+export const storage = getStorage(app);
